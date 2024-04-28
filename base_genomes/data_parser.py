02f3dae6bc46d5a10de2349fcab4ac98d65ebf9a
gsspdev <dev@gssp.io>
2024-04-27 20:03:15 -0700

    added base_genomes directory

---
+++ base_genomes/data_parser.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+import os
+
+# Read Excel file
+df = pd.read_csv('sex_quiz.csv')
+
+# Iterate over each row
+for index, row in df.iterrows():
+    # Convert the row to a dictionary
+    row_data = row.to_dict()
+    # Create a separate JSON file for each row
+    with open(f'genome_{index}.json', 'w') as f:
+        f.write(json.dumps(row_data, indent=4))
+
